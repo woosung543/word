f3703ec4eb91fa2e9e7cf564a963fa1b70d37d97
woosung543 <21700543@handong.ac.kr>
2022-09-02 10:27:13 +0900

    1st Commit

---
+++ src/com/mycom/word/WordManager.java
@@ -0,0 +1,13 @@
+package com.mycom.word;
+
+import java.util.Scanner;
+
+public class WordManager {
+	
+	Scanner sc = new Scanner();
+	
+	public void start() {
+		System.out.println("WordManager!!!");
+	}
+
+}
