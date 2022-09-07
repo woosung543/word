020ac93cf2a7cab4fe787ab0a41225002b046a45
woosung543 <21700543@handong.ac.kr>
2022-09-07 14:16:11 +0900

    3rd commit

--- src/com/mycom/word/WordCRUD.java
+++ src/com/mycom/word/WordCRUD.java
@@ -31,19 +31,16 @@ public class WordCRUD implements ICRUD{
 
 	@Override
 	public int update(Object obj) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int delete(Object obj) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public void selectOne(int id) {
-		// TODO Auto-generated method stub
 	}
 
 	public void listAll() {
